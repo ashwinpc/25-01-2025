dcbdca063be2d95e63ad6da05998f0058bdd7ef8
ashwinpc <93504065+ashwinpc@users.noreply.github.com>
2025-01-25 16:30:40 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1IndexerDemo.Model
+{
+     // step 1
+    public class Employee
+    {
+        // data members
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Employee(int id, string name)
+        {
+           this.Id = id;
+            this.Name = name;
+        }
+        // step 2  : Creater Indexer
+        // modifier returnType this [int/string]
+        public object this[int index]
+        {
+            // get Accessor
+            get 
+            {
+                if (index == 0)
+
+                    return Id;
+
+                else if (index == 1)
+                    return Name;
+
+                else return null;
+            }
+            // set Accessor
+            set
+            {
+
+                if (index == 0)
+
+                    Id=Convert.ToInt32(value);
+
+                else if (index == 1)
+                     Name=value.ToString();
+
+               
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp1IndexerDemo.Model;
+
+namespace ConsoleApp1IndexerDemo
+{
+    internal class Program
+    {
+        // indexer in C # provide a convinent way to access element of a collection or class instance in a similar
+        //manner to arrays here are some adavanages of using indexers:
+        //Simplified Syntax:Indexers allow you to acess elements of a collection or classusig array-like syntax([]).
+        // with indexers:Directly access elements using array like syntax
+        //without indesxer:requires explicit method or properties calls to acces elements,indexers offer a more concise readable,and encapusalte
+        //way to access elements within a class
+        static void Main(string[] args)
+        {
+            // Step 3 --- GetInput from the user
+            Console.Write("ENTER id :");
+            int empId=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ener NAme");
+            string empName=Console.ReadLine();
+            // create an instance
+            Employee  emp= new Employee(empId,empName);
+            // output resut
+            Console.WriteLine("Emp id : " + emp[0]);
+            Console.WriteLine("Emp NAme: "+ emp[1]);
+            Console.WriteLine("press any key to continue ........");
+            Console.ReadKey();
+        }
+    }
+}
