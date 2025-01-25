c61850bfe26950dd313913b542da7f87b1a6f865
ashwinpc <93504065+ashwinpc@users.noreply.github.com>
2025-01-25 16:33:04 +0530

    Add files via upload
    Both IMusic ICamera are interface from the Serivce folder

---
+++ ICamera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAbstraction.Serivce
+{
+    // Interfcae for camera functionality
+    public interface ICamera
+    {
+        void TakePhoto();
+        void RecordVideo();
+    }
+}
---
+++ IMusicPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAbstraction.Serivce
+{
+    public interface IMusicPlayer
+    {
+        // public interface for music player functionality
+        void PlayMusic(string trackName);
+        void StopMusic();
+    }
+}
