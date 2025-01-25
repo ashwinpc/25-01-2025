d1fc7f5800b00d3919c1acc110c1a13fdedb4b83
ashwinpc <93504065+ashwinpc@users.noreply.github.com>
2025-01-25 16:37:56 +0530

    Program.cs file of the interface question

---
+++ MobileFactory.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleAppAbstraction.Model;
+
+namespace ConsoleAppAbstraction
+{
+    internal class MobileFactory
+    {
+        static void Main(string[] args)
+        {
+            //Create a basic  instance
+            BasicPhone basicPhone = new BasicPhone();
+            basicPhone.Brand = "Nokia";
+            basicPhone.Model = "3310";
+            basicPhone.Price = 5000;
+            BasicPhone basicPhone2 = new BasicPhone
+            {
+                Brand = "Nokia",
+                Model = "3312",
+                Price=4500
+
+
+
+            };
+            basicPhone.DisplayDetails();
+            basicPhone.MakeCall("923345456");
+            basicPhone.SendMessage("923345456", "hello from basicc phone !");
+            // create a smarthphone instance
+            SmartPhone smartPhone = new SmartPhone();
+            smartPhone.Brand = "samsung";
+            smartPhone.Model = "S23";
+            smartPhone.Price = 1000;
+            smartPhone.DisplayDetails();
+            smartPhone.MakeCall("1023345456");
+            smartPhone.SendMessage("1233345456", "hello from smartphone phone !");
+            smartPhone.TakePhoto();
+            smartPhone.RecordVideo();
+            smartPhone.PlayMusic("Faviortes song");
+            smartPhone.StopMusic();
+
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
